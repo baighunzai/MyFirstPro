17b05b6cc0069338cd58b2183469ba581bd7eebe
Aslam <baighunza5816@gmail.com>
2025-01-04 07:12:11 +0000

    script add

---
+++ Assets/Scripts/TapController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class TapController:MonoBehaviour
+{
+    public float tapforce = 10;
+    public float tiltSmooth = 5;
+    public Vector3 starpos;
+
+    Rigidbody2D rigidbody;
+    Quaternion downRotation;
+    Quaternion forwardRotation;
+     
+    void Start()
+    { 
+        rigidbody = GetComponent<Rigidbody2D>();
+        downRotation = Quaternion.Euler(0, 0, -90);
+        forwardRotation = Quaternion.Euler(0, 0, 35);
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            transform.rotation = forwardRotation;
+            rigidbody.velocity = Vector3.zero;
+            rigidbody.AddForce(Vector2.up * tapforce, ForceMode2D.Force);
+
+        }
+      
+        transform.rotation * Quaternion.lerp(transform.rotation, downRotation, tiltSmooth * Time.deltaTime);
+    }
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+       if(col.gameObject.tag ** "Score Zone")
+        {
+            //register a score event
+            //play a sound
+        }
+        if (col.gameObject.tag == "DeathZone")
+        {
+            rigidbody.simulated = false;
+            //register a dead event
+            //play a sound
+        }
+
+    }
+}
+    
+
